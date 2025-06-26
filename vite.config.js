71b324f1d120c60c34ee06dec7c86473c98015ef
gerrygeop <gerryja28@gmail.com>
2025-06-26 20:53:21 +0800

    chore: update base & scripts run

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: "/gerrygeop.github.io/",
+	base: "/",
 	plugins: [vue(), vueDevTools(), tailwindcss()],
 	resolve: {
 		alias: {
